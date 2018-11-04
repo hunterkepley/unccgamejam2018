7403c3a28b28661018e982f3da14da1aee46e596
hunterkepley <kepley.l.hunter@gmail.com>
2018-11-04 10:24:29 -0500

    Unfinished shelf minigame

--- src/shelfminigame.rs
+++ src/shelfminigame.rs
@@ -4,7 +4,11 @@ use super::minigame;
 use super::timebar;
 
 pub struct ShelfMinigame {
-
+    pub shelf_image: graphics::Image,
+    pub trophy_clean_image: graphics::Image,
+    pub trophy_dirty_image: graphics::Image,
+    pub trophy_batch: graphics::spritebatch::SpriteBatch,
+    
 }
 
 impl ShelfMinigame {
